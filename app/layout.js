14546009a007e476204378d134b9326186b04e2c
Phung Nguyen <30420711+longphung@users.noreply.github.com>
2024-04-10 15:32:25 +1000

    update-dependabot-config (#5)
    * ✨ Update dependabot configuration for github actions.
    
    * 🔄 Configure Dependabot for npm updates.
    
    * ✨ Improve layout.js structure and add logo to header.
    
    * ✨ Add @mui/base package for base components and utilities.
    
    * ✨ Improve link styling in Header component.

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,52 @@
-import { Montserrat } from "next/font/google";
+import {Montserrat} from "next/font/google";
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '../public/logo.png'
 import "./globals.css";
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({subsets: ["latin"]});
 
 export const metadata = {
   title: "TechAway",
   description: "Professional IT Solutions by Tech Away. Small Business, Managed IT or Non-Profit Organisation. Our Expert Team is ready to Provide IT Solution any-day 24x7.",
 };
 
-export default function RootLayout({ children }) {
+const Header = () => {
+  return (
+    <header className='bg-white flex justify-between px-32 py-8'>
+      <Image
+        src={logo}
+        alt="TechAway Logo"
+        width={277}
+        height={57}
+      />
+      <nav className='flex items-center'>
+        <ul className='flex justify-between items-center gap-12 font-bold text-black'>
+          <li className='flex items-center hover:underline decoration-2 underline-offset-8'>
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li className='flex items-center hover:underline decoration-2 underline-offset-8'>
+            <Link href="/schedule">Schedule</Link>
+          </li>
+          <li className='flex items-center hover:underline decoration-2 underline-offset-8'>
+            <Link href="/tickets">Tickets</Link>
+          </li>
+          <li className='flex items-center hover:underline decoration-2 underline-offset-8'>
+            <Link href="/shop">Shop</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+    <body className={montserrat.className}>
+    <Header/>
+    {children}
+    </body>
     </html>
   );
 }
