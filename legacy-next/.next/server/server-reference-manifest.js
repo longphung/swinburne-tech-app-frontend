c7e0407c62bc86047101fddfcc112910fc39397a
Phung Nguyen <longphung.nn@gmail.com>
2024-04-18 09:39:19 +1000

    move current frontend to legacy

---
+++ legacy-next/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"FNAYnB+7TR4oUvtk/q5/GotJxoEWAjrJOztkK9xrUsg=\"}"
---
+++ legacy-next/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};
+
+export default nextConfig;
