5dda74d961b85d462d9268d485b91e6f319779f0
Phung Nguyen <longphung.nn@gmail.com>
2024-04-24 14:53:30 +1000

    ✨ feat: implement user list

--- src/pages/dashboard/users/list.tsx
+++ src/pages/dashboard/users/list.tsx
@@ -1,5 +1,31 @@
-const UsersList = () => {
-  return (<>UsersList</>)
-}
+import { DataGrid } from "@mui/x-data-grid";
+import { List, useDataGrid } from "@refinedev/mui";
 
-export default UsersList
+const UsersList = () => {
+  const { dataGridProps } = useDataGrid({
+    resource: "users",
+    sorters: {
+      initial: [{ field: "name", order: "asc" }],
+    },
+  });
+
+  return (
+    <List title="Users" resource="users">
+      <DataGrid
+        {...dataGridProps}
+        columns={[
+          { field: "id", headerName: "ID", width: 90 },
+          { field: "name", headerName: "Name", width: 150 },
+          { field: "username", headerName: "Username", width: 150 },
+          { field: "email", headerName: "Email", width: 150 },
+          { field: "role", headerName: "Role", width: 150 },
+          { field: "address", headerName: "Address", width: 150 },
+          { field: "phone", headerName: "Phone", width: 150 },
+        ]}
+        autoHeight
+      />
+    </List>
+  );
+};
+
+export default UsersList;
