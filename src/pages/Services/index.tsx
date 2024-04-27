ef8cc51744d9618d0caebcebfd1ddb794656f83f
Phung Nguyen <longphung.nn@gmail.com>
2024-04-27 15:34:13 +1000

    ✨ feat: add debounced search

--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -2,13 +2,23 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import { useList } from "@refinedev/core";
-import { ServiceData } from "@/interfaces";
-import { useState } from "react";
+import { ChangeEvent, useMemo, useState } from "react";
 import ServiceShoppingCard from "@/components/ServiceShoppingCard";
+import { debounce, Pagination } from "@mui/material";
+
+import { ServiceData } from "@/interfaces";
 
 const Services = () => {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+
   const { data } = useList({
     resource: "services",
+    pagination: {
+      current: page,
+      pageSize: 1,
+    },
+    meta: { query: { q: search } },
   });
   const usableData =
     (data as unknown as {
@@ -26,10 +36,24 @@ const Services = () => {
         totalPages: number;
       };
     }) || {};
+  const debouncedSetSearch = useMemo(
+    () =>
+      debounce((searchTerm) => {
+        setSearch(searchTerm);
+        setPage(1);
+      }, 250),
+    [],
+  );
 
-  const [search, setSearch] = useState("");
+  // console.log(usableData);
 
-  console.log(usableData);
+  const handlePageChange = (_event: ChangeEvent<unknown>, page: number) => {
+    setPage(page);
+  };
+
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    debouncedSetSearch(event.target.value);
+  };
 
   return (
     <Container
@@ -38,9 +62,9 @@ const Services = () => {
         padding: "2rem",
       }}
     >
-      <Grid container>
+      <Grid container spacing={4}>
         <Grid container item xs={12} display="flex" justifyContent="flex-end">
-          <TextField label="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+          <TextField label="Search" onChange={handleSearchChange} />
         </Grid>
 
         <Grid container item xs={12} spacing={2}>
@@ -48,6 +72,20 @@ const Services = () => {
             <ServiceShoppingCard key={service.id} service={service} />
           ))}
         </Grid>
+
+        {usableData?.data?.totalPages > 1 && (
+          <Grid container item xs={12}>
+            <Pagination
+              color="primary"
+              onChange={handlePageChange}
+              count={usableData?.data?.totalPages}
+              page={page}
+              sx={{
+                margin: "auto",
+              }}
+            />
+          </Grid>
+        )}
       </Grid>
     </Container>
   );
