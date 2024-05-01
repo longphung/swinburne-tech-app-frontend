37506757681bfd88f6e248686bf3546acc4c6b85
Phung Nguyen <longphung.nn@gmail.com>
2024-05-01 14:01:07 +1000

    ✨ feat: update Homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,42 @@
 import React from "react";
-import { Box, Card, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { useList } from "@refinedev/core";
+
+import { ServiceData } from "@/interfaces";
+import ServiceShoppingCard from "@/components/ServiceShoppingCard";
 
 const HomePage = () => {
+  const { data } = useList({
+    resource: "services",
+    pagination: {
+      current: 1,
+      pageSize: 3,
+    },
+  });
+  const usableData =
+    (data as unknown as {
+      data: {
+        docs: ServiceData[];
+        hasNextPage: boolean;
+        hasPrevPage: boolean;
+        limit: number;
+        nextPage: number;
+        offset: number;
+        page: number;
+        pageCounter: number;
+        prevPage: number;
+        totalDocs: number;
+        totalPages: number;
+      };
+    }) || {};
   return (
     <Box>
-      {/* Section 1: Welcome Section with Background Image */}
       <Box
         sx={{
-          backgroundImage: "url('/background.jpg')", // URL to your background image
+          backgroundImage: "url('/background.jpg')",
           backgroundSize: "cover",
           backgroundPosition: "center",
-          height: "600px", // Adjust height as needed
+          height: "600px",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -26,50 +52,16 @@ const HomePage = () => {
           </Typography>
         </Container>
       </Box>
-
-      {/* Section 2: Our Top Services */}
       <Container>
         <Typography variant="h3" align="center" mt={6} mb={4}>
           Our Top Services
         </Typography>
-        {/* Grid to display services */}
         <Grid container spacing={4} justifyContent="center">
-          {/* Replace with your service components */}
-          <Grid item>
-            <Card>
-              <CardMedia
-                component="img"
-                height="140"
-                image={"/mic.png"} // URL of your image
-              />
-              <CardContent style={{ textAlign: "justify" }}>
-                <Typography variant="subtitle1" component="div">
-                  Set Up My Email Account
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <Typography variant="body2">We&apos;ll assist you with</Typography>
-                  <Typography variant="body2">setting up and configuring</Typography>
-                  <Typography variant="body2">your email accounts on various</Typography>
-                  <Typography variant="body2">email clients or devices.</Typography>
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Box>
-              <Typography variant="h5">Service 2</Typography>
-              <Typography variant="body1">Description of Service 2</Typography>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Box>
-              <Typography variant="h5">Service 3</Typography>
-              <Typography variant="body1">Description of Service 3</Typography>
-            </Box>
-          </Grid>
+          {usableData?.data?.docs.map((service: ServiceData) => (
+            <ServiceShoppingCard service={service} key={service.id} />
+          ))}
         </Grid>
       </Container>
-      {/* Section 3: Any other info */}
       <Container>
         <Typography variant="h3" align="center" mt={6} mb={4}>
           Any other info
