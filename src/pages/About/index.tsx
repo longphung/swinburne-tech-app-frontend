f45ffb39ea27794e28a1f55f2e4b7e26d79ea17d
BashiniDewage <bashidewagee@gmail.com>
2024-05-11 10:12:27 +1000

    about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,8 +1,249 @@
-const About = () => {
+/*const About = () => {
   return (
     <div>
       <h1>About</h1>
     </div>
+  );
+};
+
+export default About;
+*/
+
+import React from "react";
+import { Container, Card, Typography, CardMedia, CardContent, Grid, Box ,IconButton,Link} from "@mui/material";
+import styled from '@emotion/styled';
+import Facebook from '@mui/icons-material/Facebook';
+import Twitter from '@mui/icons-material/Twitter';
+import Instagram from '@mui/icons-material/Instagram';
+import YouTube from '@mui/icons-material/YouTube';
+
+const Header = styled(Box)`
+  text-align: center;
+  margin-bottom: 40px;
+  padding: 20px;
+  background-image: url('/banner.jpeg');
+  background-size: cover;
+  background-position: center;
+  color: white;
+`;
+
+const HeaderImg = styled(CardMedia)`
+  height: 100px;
+ 
+`;
+
+const Mission = styled(Box)`
+  margin-bottom: 40px;
+`;
+
+const MissionTitle = styled(Typography)`
+  font-size: 1.5em;
+  color: #333;
+`;
+
+const MissionText = styled(Typography)`
+  font-size: 16px;
+  color: #666;
+`;
+
+const Services = styled(Box)`
+  margin-bottom: 40px;
+`;
+
+const ServicesTitle = styled(Typography)`
+  font-size: 1.5em;
+  color: #333;
+`;
+
+const ServicesText = styled(Typography)`
+  font-size: 16px;
+  color: #666;
+  margin-bottom: 10px;
+`;
+
+const OurTeam = styled(Box)`
+  margin-bottom: 40px;
+`;
+
+const OurTeamTitle = styled(Typography)`
+  font-size: 1.5em;
+  color: #333;
+`;
+
+const OurTeamText = styled(Typography)`
+  font-size: 16px;
+  color: #666;
+`;
+
+const JoinUs = styled(Box)`
+  text-align: left;
+`;
+
+const JoinUsTitle = styled(Typography)`
+  font-size: 1.5em;
+  color: #333;
+`;
+
+const JoinUsText = styled(Typography)`
+  font-size: 16px;
+  color: #666;
+`;
+
+const About = () => {
+  return (
+    
+    <Box>
+       <Box
+        sx={{
+          backgroundImage: "url('banner.jpeg')", // URL to your background image
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "600px", // Adjust height as needed
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+          
+        </Box>
+  
+      <Container>
+      <Typography variant="h4" component="h4" sx={{ paddingBottom: "40px", paddingTop: "40px", textAlign: "center" }}>
+          About 
+        </Typography>
+      <Mission>
+        <MissionTitle variant="h2" sx={{  textAlign: "center" }}>
+          Our Mission
+        </MissionTitle>
+        <MissionText variant="body1">
+          Our mission is to provide affordable, reliable, and innovative IT solutions to small businesses. We believe that every business, regardless of its size, deserves access to top-notch IT support and cloud services.
+        </MissionText>
+      </Mission>
+      <Services>
+        <ServicesTitle variant="h2" sx={{  textAlign: "center" }}>
+          Our Services
+        </ServicesTitle>
+        <ul>
+          <li>
+            <ServicesText variant="body1">
+            <b>Microsoft 365 Services </b>:
+Discover seamless assistance with Microsoft 365 from our remote services. Whether it's setting up email accounts, resolving delivery issues, enhancing security, or optimizing applications, we've got you covered. With our remote and dual options, you can efficiently harness the power of Microsoft 365 starting from $39.99.
+            </ServicesText>
+          </li>
+          <li>
+            <ServicesText variant="body1">
+            <b>WiFi Services </b>:
+Experience uninterrupted connectivity with our onsite WiFi services. From boosting signal strength and connecting devices to stopping dropouts and securing networks, we provide comprehensive solutions to ensure your WiFi works seamlessly. Prices start at $29.99 for onsite assistance.
+            </ServicesText>
+          </li>
+          <li>
+            <ServicesText variant="body1">
+            <b>Email Services</b>:
+Streamline your inbox and communication processes with our remote email services. Whether it's decluttering your inbox, fixing delivery delays, enhancing security, or optimizing speed, our expert assistance is just a click away. Prices begin at $29.99 for remote support.
+            </ServicesText>
+          </li>
+          <li>
+            <ServicesText variant="body1">
+            <b>Website Services</b>:
+Elevate your online presence with our remote website services. From designing captivating banners and fixing broken links to securing your website and optimizing speed, we offer tailored solutions to enhance user experience and engagement. Prices start at $29.99 for remote assistance.
+            </ServicesText>
+          </li>
+          <li>
+            <ServicesText variant="body1">
+            <b>Cloud Services</b>:
+Unlock the potential of cloud technology with our comprehensive remote cloud services. Whether you're migrating your business, fortifying security, optimizing costs, or setting up backup solutions, our expert guidance ensures a smooth transition to the cloud. Prices begin at $299.99 for remote support.
+            </ServicesText>
+          </li>
+          <li>
+            <ServicesText variant="body1">
+            <b>IT Security Services</b>:
+Protect your digital assets with our extensive IT security services. From fortifying networks and testing defenses to crafting policies, planning responses, and training teams, we offer holistic solutions to safeguard your organization against cyber threats. Prices start at $199.99 for comprehensive training sessions.
+            </ServicesText>
+          </li>
+          
+        </ul>
+      </Services>
+      <OurTeam>
+        <OurTeamTitle variant="h2" sx={{  textAlign: "center" }}>
+          Our Team
+        </OurTeamTitle>
+        <OurTeamText variant="body1">
+          Our team is our greatest asset. We are a group of passionate, skilled, and experienced professionals, dedicated to providing you with the best possible service.
+        </OurTeamText>
+      </OurTeam>
+      <JoinUs>
+        <JoinUsTitle variant="h2" sx={{  textAlign: "center" }}>
+          Join Us
+        </JoinUsTitle>
+        <JoinUsText variant="body1" sx={{ paddingBottom: "40px" }}>
+          We are always on the lookout for talented individuals to join our team. If you are passionate about IT and cloud solutions, we would love to hear from you.
+        </JoinUsText>
+      </JoinUs>
+
+
+     
+
+
+      </Container>
+
+      <Box bgcolor="primary.main" color="black" py={4}>
+        <Container>
+          <Grid container spacing={4}>
+            
+            <Grid item xs={12} sm={3}>
+
+              <Typography variant="h6">Navigation</Typography>
+              <ul style={{ listStyleType: 'none', padding: 0 }}>
+                <li><Link href="#" color="inherit" underline="none">Homepage</Link></li>
+                <li><Link href="#" color="inherit" underline="none">Services</Link></li>
+                <li><Link href="#" color="inherit" underline="none">About</Link></li>
+                <li><Link href="#" color="inherit" underline="none" >Login</Link></li>
+              </ul>
+            </Grid>
+            {/* Contact */}
+            <Grid item xs={12} sm={3}>
+              <Typography variant="h6">Contact</Typography>
+              <ul style={{ listStyleType: 'none', padding: 0 }}>
+                <li>Phone: 123-456-7890</li>
+                <li>Email: example@example.com</li>
+                <li>Address: 123 Main St, City</li>
+              </ul>
+            </Grid>
+
+            {/* Social Media Icons */}
+
+            <Grid item xs={12} sm={3}>
+              <Typography variant="h6" gutterBottom>Social Media</Typography>
+              <IconButton color="inherit">
+                <Facebook />
+              </IconButton>
+              <IconButton color="inherit">
+                <Twitter />
+              </IconButton>
+              <IconButton color="inherit">
+                <Instagram />
+              </IconButton>
+              <IconButton color="inherit">
+                <YouTube />
+              </IconButton>
+            </Grid>
+            {/* Admin Login */}
+            <Grid item xs={12} sm={3}>
+              <Typography variant="h6">Admin</Typography>
+              <ul style={{ listStyleType: 'none', padding: 0 }}>
+                <li><Link href="#" underline="none" color="inherit">Login</Link></li>
+              </ul>
+            </Grid>
+
+          </Grid>
+
+        </Container>
+      </Box>
+
+    </Box>
+  
+
+
+    
   );
 };
 
