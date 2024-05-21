6c93d0780f622f6711bb8d96ab08bc4c22c28e39
Phung Nguyen <longphung.nn@gmail.com>
2024-05-22 00:32:27 +1000

    ✨ feat: update features for technicians

--- src/pages/dashboard/orders/list.tsx
+++ src/pages/dashboard/orders/list.tsx
@@ -1,4 +1,5 @@
 import { List, useDataGrid } from "@refinedev/mui";
+import ReceiptIcon from "@mui/icons-material/Receipt";
 import Typography from "@mui/material/Typography";
 import { Breadcrumbs, Chip, Link } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
@@ -6,7 +7,26 @@ import { Link as RouterLink } from "react-router-dom";
 import { format } from "date-fns";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
+
 import DataGridActionCell from "@/components/DataGridActionCell";
+import Button from "@mui/material/Button";
+import { getOrderPDFInvoice } from "@/api/backend";
+
+const DownloadInvoiceButton = ({ id }: { id: string }) => {
+  return (
+    <Button
+      variant="contained"
+      onClick={() => {
+        getOrderPDFInvoice(id).then((res) => {
+          window.open(res);
+        });
+      }}
+      startIcon={<ReceiptIcon />}
+    >
+      Generate Invoice
+    </Button>
+  );
+};
 
 const OrdersList = () => {
   const { dataGridProps } = useDataGrid({
@@ -85,6 +105,13 @@ const OrdersList = () => {
                 <Chip color="error" label="Cancelled" />
               );
             },
+          },
+          {
+            field: "download",
+            headerName: "Download Invoice",
+            type: "actions",
+            width: 250,
+            renderCell: (params) => <DownloadInvoiceButton id={params.row.id} />,
           },
           {
             field: "actions",
---
+++ src/pages/dashboard/orders/show.tsx
@@ -0,0 +1,72 @@
+import { Show } from "@refinedev/mui";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Document, Page } from "react-pdf";
+import { Breadcrumbs, CircularProgress, Link } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { Link as RouterLink } from "react-router-dom";
+import { FC, useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useOne } from "@refinedev/core";
+
+import { OrderData } from "@/interfaces";
+import { getOrderPDFInvoice } from "@/api/backend";
+
+const OrdersShow: FC = () => {
+  const loadingPdf = useRef(false);
+  const { id } = useParams();
+  const [pdfURL, setPdfURL] = useState<null | string>(null);
+
+  useEffect(() => {
+    if (!loadingPdf.current && id) {
+      loadingPdf.current = true;
+      getOrderPDFInvoice(id).then((res) => {
+        setPdfURL(res);
+        loadingPdf.current = false;
+      });
+    }
+    return () => {
+      loadingPdf.current = false;
+    };
+  }, []);
+
+  const breadcrumb = (
+    <Breadcrumbs aria-label="breadcrumb">
+      <Link underline="hover" color="inherit" component={RouterLink} to="/dashboard">
+        Dashboard
+      </Link>
+      <Link underline="hover" color="inherit" component={RouterLink} to="/dashboard/orders">
+        Orders
+      </Link>
+      <Typography color="text.primary">{id}</Typography>
+    </Breadcrumbs>
+  );
+
+  return (
+    <Show breadcrumb={breadcrumb} resource="orders" recordItemId={id}>
+      {pdfURL ? (
+        <>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="h4">Invoice:</Typography>
+            <Button variant="contained" component="a" href={pdfURL} target="_blank" rel="noreferrer">
+              Download Invoice
+            </Button>
+          </Box>
+          {/* TODO: known issues with pdf blanking when switching tab https://github.com/wojtekmaj/react-pdf/issues/1798 */}
+          <Document file={pdfURL}>
+            <Page pageNumber={1} />
+          </Document>
+        </>
+      ) : (
+        <CircularProgress />
+      )}
+    </Show>
+  );
+};
+
+export default OrdersShow;
