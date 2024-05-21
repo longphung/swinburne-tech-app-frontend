480d87d33ee38b5faa21d02439419500c7e36b05
Phung Nguyen <30420711+longphung@users.noreply.github.com>
2024-05-21 20:56:50 +1000

    ✨ feat: add sample tests (#54)

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   projectId: "sq5j3e",
   retries: 3,
   e2e: {
-    fixturesFolder: "../../cypress/fixtures",
-    supportFile: "../../cypress/support/e2e.ts",
+    fixturesFolder: "./cypress/fixtures",
+    supportFile: "./cypress/support/e2e.ts",
   },
   chromeWebSecurity: false,
   experimentalMemoryManagement: true,
